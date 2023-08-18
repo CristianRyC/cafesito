3314b932eba0aa1e0af966e9c717e3032e5e59e3
CristianRyC <cristian2.programacion@gruporyc.com.mx>
2023-08-18 16:55:18 -0600

    fix header, respons

--- src/componentes/atomos/Header.jsx
+++ src/componentes/atomos/Header.jsx
@@ -14,39 +14,36 @@ function Header() {
 
   return (
     <>
-      <div>
-
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-          <div class="container-fluid"><div className='logo'>
-            <img src={Logo} alt="logo" />
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid"><div className='logo'>
+          <img src={Logo} alt="logo" />
+        </div>
+          <Link class="navbar-brand" to="/">PEKO</Link>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <Link class="nav-link" aria-current="page" to="/">HOME</Link>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link " href="#conocenos">CONOCENOS</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link " href="#footer">CONTACTANOS</a>
+              </li>
+              <li class="nav-item " >
+                <a class="nav-link " onClick={handleShow}>REGISTRATE</a>
+              </li>
+            </ul>
           </div>
-            <Link class="navbar-brand" to="/">PEKO</Link>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                  <Link class="nav-link" aria-current="page" to="/">HOME</Link>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link " href="#conocenos">CONOCENOS</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link " href="#footer">CONTACTANOS</a>
-                </li>
-                <li class="nav-item " >
-                  <a class="nav-link " onClick={handleShow}>REGISTRATE</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-      </div>
+        </div>
+      </nav>
 
 
 
- {/*      <div className="navbar">
+      {/*      <div className="navbar">
         <div className='logo'>
           <img src={Logo} alt="logo" />
           <h1>Peko</h1>
