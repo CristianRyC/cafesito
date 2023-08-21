269eee3466ac8a5f0febe50b1821a899135cc930
CristianRyC <cristian2.programacion@gruporyc.com.mx>
2023-08-21 11:55:21 -0600

    fix

--- src/componentes/atomos/Header.jsx
+++ src/componentes/atomos/Header.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { Link } from "react-router-dom";
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
@@ -6,11 +6,69 @@ import Modal from 'react-bootstrap/Modal';
 import ScrollToTop from "react-scroll-to-top";
 import Logo from "../../assets/images/LogoPeko.png"
 import '../../assets/styles/HeaderStyle.css'
+import Swal from "sweetalert2";
 
 function Header() {
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+  const formdata = useRef();
+
+  const handlerClick = (e) => {
+    e.preventDefault();
+    const formData = new FormData(formdata.current);
+    const correo = formData.get("correo");
+    fetch("http://44.218.190.25:3000/Usuarios")
+      .then((response) => response.json())
+      .then((correosExistentes) => {
+        const listaCorreos = correosExistentes.map((correos) => correos.Correo);
+        if (listaCorreos.includes(correo)) {
+          Swal.fire({
+            icon: "error",
+            title: "Error al registrar usuario",
+            text: `El correo  ${correo}  ya está en uso.`,
+          });
+        } else {
+          const URI = "http://44.218.190.25:3000/Usuarios";
+          const opciones = {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              Nombre: formData.get("name"),
+              Telefono: formData.get("telefono"),
+              Correo: correo,
+            }),
+          };
+          fetch(URI, opciones)
+            .then((response) => response.json())
+            .then((data) => {
+              if (data.message === "Usuario registrado exitosamente") {
+                Swal.fire({
+                  icon: "success",
+                  text: `Correo registrado: ${correo}`,
+                  timer: 1500,
+                }).then(() => {
+                  setShow(false);
+                });
+              } else {
+                Swal.fire({
+                  icon: "error",
+                  title: "Error al registrar usuario",
+                  text: data.error,
+                });
+              }
+            })
+            .catch((error) => {
+              console.error("Error:", error);
+            });
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
 
   return (
     <>
@@ -41,46 +99,28 @@ function Header() {
         </div>
       </nav>
 
-
-
-      {/*      <div className="navbar">
-        <div className='logo'>
-          <img src={Logo} alt="logo" />
-          <h1>Peko</h1>
-        </div>
-
-        <div>
-          <Link to="/" className='sinlinea'><button className='boton'>HOME</button></Link>
-          <a href="#conocenos" className='sinlinea'><button className='boton'>CONOCENOS</button></a>
-          <a href="#footer" className='sinlinea'><button className='boton'>CONTACTO</button></a>
-          <button onClick={handleShow} className='boton'>REGISTRAR</button>
-        </div>
-      </div>
- */}
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
           <Modal.Title>Registrate</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form>
+          <Form ref={formdata}>
             <Form.Group className="mb-1" controlId="exampleForm.ControlInput1">
-              {/* <Form.Label>:)</Form.Label> */}
               <Form.Control
                 type="text"
                 placeholder="Nombre"
-                autoFocus
-              />
-              <br />
+                name='name'
+                autoFocus /><br />
               <Form.Control
                 type="telephone"
                 placeholder="Telefono"
-                autoFocus
-              /> <br />
+                name='telefono'
+                autoFocus /> <br />
               <Form.Control
                 type="email"
                 placeholder="Correo"
-                autoFocus
-              />
+                name='correo'
+                autoFocus />
             </Form.Group>
           </Form>
         </Modal.Body>
@@ -88,7 +128,7 @@ function Header() {
           <Button variant="secondary" onClick={handleClose}>
             Cerrar
           </Button>
-          <Button variant="primary" onClick={handleClose}>
+          <Button variant="primary" onClick={handlerClick}>
             Enviar
           </Button>
         </Modal.Footer>
